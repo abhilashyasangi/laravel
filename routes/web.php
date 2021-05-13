7dd3a0c39cf320cacbf1c0608ff0ce05dbdddd6c
abhilashyasangi <84038830+abhilashyasangi@users.noreply.github.com>
2021-05-13 22:30:23 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,23 @@
+<?php
+
+Route::get('/games', 'GamesController@index');
+
+Route::get('/games/create', 'GamesController@create');
+
+Route::get('/games/{game}', 'GamesController@show');
+
+Route::post('/games', 'GamesController@store');
+
+Route::post('/games/{game}/reviews', 'ReviewsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionsController@create');
+Route::post('/login', 'SessionsController@store');
+Route::get('/logout', 'SessionsController@destroy'); 
+ 
+ 
+ 
+ 
 
